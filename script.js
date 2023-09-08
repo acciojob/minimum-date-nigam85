b7d29631e33f69ea479cdf50569d6f9a6c7e20af
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-08 12:01:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function minDate(dates) {
   //write you code here
+	const dateObjects = dates.map(dateStr => new Date(dateStr));
+	 // Find the minimum date using Math.min() and apply Date.getTime()
+  const minDateObject = new Date(Math.min(...dateObjects.map(date => date.getTime())));
+
+	  // Format the minimum date as "YYYY/MM/DD"
+  const year = minDateObject.getFullYear();
+  const month = String(minDateObject.getMonth() + 1).padStart(2, '0'); // Add 1 to month since it's zero-based
+  const day = String(minDateObject.getDate()).padStart(2, '0');
+
+  return `${year}/${month}/${day}`;
+
 }
 
 // Do not change the code
